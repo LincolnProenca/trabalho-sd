d077a21f3c7327f5af5c86a6ceab6cd2ef57926e
Lincoln Rodrigues Proença <91763571+LincolnProenca@users.noreply.github.com>
2023-07-18 14:58:57 -0300

    Update placa.vhd

--- placa.vhd
+++ placa.vhd
@@ -2,17 +2,21 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+
 entity placa is
 	port(
 		OPERATOR: in std_logic_vector(2 downto 0);
         CLOCK_50: in std_logic;
         G_HEX0: out std_logic_vector(6 downto 0);
-		RESULT: out std_logic_vector(7 downto 0);
+		RESULT: out std_logic_vector(7 downto 0)
 	);
 end placa;
 
 architecture behavioral of placa is
-	signal counter: unsigned(3 downto 0) := "0000";
+	signal counterAux: unsigned(3 downto 0) := "0000";
+    signal counter: std_logic_vector(3 downto 0) := "0000";
+    signal flagCarry, flagNegativo, flagZero, flagOverflow: std_logic;
+    signal resultOp: std_logic_vector(3 downto 0);
     signal const: std_logic_vector(3 downto 0);
 
     component counter_seconds is
@@ -23,8 +27,8 @@ architecture behavioral of placa is
 
     component decoder7seg is
         port (
-            bcd_in: in unsigned(3 downto 0) := "0000";
-            seven_seg_out: out std_logic_vector(6 downto 0)
+            entrada: in std_logic_vector(3 downto 0) := "0000";
+            numeroDisplay: out std_logic_vector(6 downto 0)
         );
     end component;
 
@@ -38,15 +42,16 @@ architecture behavioral of placa is
 	end component;
 
 begin
-	counter0: counter_seconds port map(CLOCK_50, counter);
+	counter0: counter_seconds port map(CLOCK_50, counterAux);
+    counter <= std_logic_vector(counterAux);
 
 	const <= "0010";
 
-	calculo: ula port map(counter, const, OPERATOR, result, flagCarry, flagNegativo, flagZero, flagOverflow);
+	calculo: ula port map(counter, const, OPERATOR, resultOp, flagCarry, flagNegativo, flagZero, flagOverflow);
 
     decoder0: decoder7seg port map(counter, G_HEX0);
 	
-    RESULT(3 downto 0) <= result;
+    RESULT(3 downto 0) <= resultOp;
     RESULT(4) <= flagOverflow;
     RESULT(5) <= flagCarry;
     RESULT(6) <= flagZero;

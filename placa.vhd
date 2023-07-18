be1895057cc402af0f13ff114a2b5ea263bffbe0
Lincoln Rodrigues Proença <91763571+LincolnProenca@users.noreply.github.com>
2023-07-18 15:00:25 -0300

    Update placa.vhd

--- placa.vhd
+++ placa.vhd
@@ -2,26 +2,26 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-
 entity placa is
-	port(
-		OPERATOR: in std_logic_vector(2 downto 0);
-        CLOCK_50: in std_logic;
-        G_HEX0: out std_logic_vector(6 downto 0);
-		RESULT: out std_logic_vector(7 downto 0)
-	);
+    port(
+        OPERATOR: in std_logic_vector(2 downto 0); -- Seleção do operador
+        CLOCK_50: in std_logic; -- Sinal de clock de 50 MHz
+        G_HEX0: out std_logic_vector(6 downto 0); -- Saída para o display de 7 segmentos
+        RESULT: out std_logic_vector(7 downto 0) -- Saída para o resultado e as bandeiras
+    );
 end placa;
 
 architecture behavioral of placa is
-	signal counterAux: unsigned(3 downto 0) := "0000";
-    signal counter: std_logic_vector(3 downto 0) := "0000";
-    signal flagCarry, flagNegativo, flagZero, flagOverflow: std_logic;
-    signal resultOp: std_logic_vector(3 downto 0);
-    signal const: std_logic_vector(3 downto 0);
+    signal counterAux: unsigned(3 downto 0) := "0000"; -- Sinal para o contador auxiliar
+    signal counter: std_logic_vector(3 downto 0) := "0000"; -- Sinal para o contador
+    signal flagCarry, flagNegativo, flagZero, flagOverflow: std_logic; -- Sinais para as bandeiras
+    signal resultOp: std_logic_vector(3 downto 0); -- Sinal para o resultado da operação
+    signal const: std_logic_vector(3 downto 0); -- Sinal para a constante utilizada
 
     component counter_seconds is
-        port(CLOCK_50: in std_logic;
-        counter_out: out unsigned(3 downto 0) := "0000"
+        port (
+            CLOCK_50: in std_logic;
+            counter_out: out unsigned(3 downto 0) := "0000"
         );
     end component;
 
@@ -32,29 +32,34 @@ architecture behavioral of placa is
         );
     end component;
 
-	component ula
-		port(
-			entrada1, entrada2: in std_logic_vector(3 downto 0);
-			operador: in std_logic_vector(2 downto 0);
-			result: out std_logic_vector(3 downto 0);
-			flagCarry, flagNegativo, flagZero, flagOverflow: out std_logic
-		);
-	end component;
+    component ula is
+        port (
+            entrada1, entrada2: in std_logic_vector(3 downto 0);
+            operador: in std_logic_vector(2 downto 0);
+            result: out std_logic_vector(3 downto 0);
+            flagCarry, flagNegativo, flagZero, flagOverflow: out std_logic
+        );
+    end component;
 
 begin
-	counter0: counter_seconds port map(CLOCK_50, counterAux);
+    -- Contador
+    counter0: counter_seconds port map(CLOCK_50, counterAux);
     counter <= std_logic_vector(counterAux);
 
-	const <= "0010";
+    -- Constante
+    const <= "0010";
 
-	calculo: ula port map(counter, const, OPERATOR, resultOp, flagCarry, flagNegativo, flagZero, flagOverflow);
+    -- ULA
+    calculo: ula port map(counter, const, OPERATOR, resultOp, flagCarry, flagNegativo, flagZero, flagOverflow);
 
+    -- Decodificador de 7 segmentos
     decoder0: decoder7seg port map(counter, G_HEX0);
-	
+
+    -- Saídas
     RESULT(3 downto 0) <= resultOp;
     RESULT(4) <= flagOverflow;
     RESULT(5) <= flagCarry;
     RESULT(6) <= flagZero;
-    RESULT(7) <= flagNegativo;	
-	
+    RESULT(7) <= flagNegativo;  
+
 end behavioral;

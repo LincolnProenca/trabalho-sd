c6962d1a2a5e4b44db007b0412dea647e2a2003f
Lincoln Rodrigues Proença <91763571+LincolnProenca@users.noreply.github.com>
2023-07-17 21:24:53 -0300

    Update ULA.vhd

--- ULA.vhd
+++ ULA.vhd
@@ -2,39 +2,40 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity ula is
-	port(
-		entrada1, entrada2: in std_logic_vector(3 downto 0);
-		operador: in std_logic_vector(2 downto 0);
-		result: out std_logic_vector(3 downto 0);
-		flagCarry, flagNegativo, flagZero, flagOverflow: out std_logic
-	);
+    port(
+        entrada1, entrada2: in std_logic_vector(3 downto 0); -- Entradas de 4 bits
+        operador: in std_logic_vector(2 downto 0); -- Seleção do operador
+        result: out std_logic_vector(3 downto 0); -- Saída de 4 bits
+        flagCarry, flagNegativo, flagZero, flagOverflow: out std_logic -- Sinais de bandeira
+    );
 end ula;
 
 architecture behavioral of ula is
-	signal resultFinal, resultadoSoma, resultadoSubtracao, resultadoIncremento, resultadoCA2 : std_logic_vector(3 downto 0);
-	signal resultadoAnd, resultadoComparador, resultadoMultiplicacao, resultadoQuadrado: std_logic_vector(3 downto 0);
-	signal carrySoma, carrySubtracao, carryIncremento: std_logic;
-	signal overflowSoma, overflowSubtracao, overflowIncremento, overflowMultiplicacao, overflowQuadrado: std_logic;
-	signal notEntrada2: std_logic_vector(3 downto 0);
+	-- Sinais auxiliares
+    signal resultFinal, resultadoSoma, resultadoSubtracao, resultadoIncremento, resultadoCA2: std_logic_vector(3 downto 0);
+    signal resultadoAnd, resultadoComparador, resultadoMultiplicacao, resultadoQuadrado: std_logic_vector(3 downto 0);
+    signal carrySoma, carrySubtracao, carryIncremento: std_logic;
+    signal overflowSoma, overflowSubtracao, overflowIncremento, overflowMultiplicacao, overflowQuadrado: std_logic;
+    signal notEntrada2: std_logic_vector(3 downto 0);
 
-	component incremento
-		Port(
-			entrada: in std_logic_vector(3 downto 0);
-			resultado: out std_logic_vector(3 downto 0);
-			carryOut: out std_logic;
-			flagOverflow: out std_logic
-		);
-	end component;
+    component incremento is
+        Port(
+            entrada: in std_logic_vector(3 downto 0);
+            resultado: out std_logic_vector(3 downto 0);
+            carryOut: out std_logic;
+            flagOverflow: out std_logic
+        );
+    end component;
 
-	component comparador is
-  	 	 Port ( 
-      	        a: in std_logic_vector(3 downto 0);
-         		b: in std_logic_vector(3 downto 0);
-			resultado: out std_logic_vector(3 downto 0)
-    		 );
-	end component;
+    component comparador is
+        Port ( 
+            a: in std_logic_vector(3 downto 0);
+            b: in std_logic_vector(3 downto 0);
+            resultado: out std_logic_vector(3 downto 0)
+        );
+    end component;
 
-	component FA4bits is
+    component FA4bits is
         Port (
             A: in STD_LOGIC_VECTOR(3 downto 0);
             B: in STD_LOGIC_VECTOR(3 downto 0);
@@ -43,14 +44,14 @@ architecture behavioral of ula is
             Cout: out STD_LOGIC;
             Overflow: out STD_LOGIC
         );
-	end component;
+    end component;
 
-	component CA2 is
+    component CA2 is
         Port(
             input: in std_logic_vector(3 downto 0);
             output: out std_logic_vector(3 downto 0)
         );
-	end component;
+    end component;
 
     component multiplier is 
         Port (
@@ -59,47 +60,65 @@ architecture behavioral of ula is
             overflow: out std_logic;
             p: out std_logic_vector(3 downto 0)
         );
-	end component;
+    end component;
 
 begin
-	notEntrada2 <= not entrada2;
+    notEntrada2 <= not entrada2;
 
-	soma: FA4bits port map(entrada1, entrada2, '0', resultadoSoma, carrySoma, overflowSoma);
-	subtracao: FA4bits port map(entrada1, notEntrada2, '1', resultadoSubtracao, carrySubtracao, overflowSubtracao);
-	incremento1: incremento port map(entrada1, resultadoIncremento, carryIncremento, overflowIncremento);
-	ComplementoA2: CA2 port map(entrada1, resultadoCA2);
-	resultadoAnd <= entrada1 and entrada2;
-	compara: comparador port map(entrada1,entrada2,resultadoComparador);
-	multiplica: multiplier port map(entrada1,entrada2,overflowMultiplicacao,resultadoMultiplicacao);
-	quadrado: multiplier port map(entrada1,entrada1,overflowQuadrado,resultadoQuadrado);
+    -- Soma
+    soma: FA4bits port map(entrada1, entrada2, '0', resultadoSoma, carrySoma, overflowSoma);
 
-	with operador select
-		resultFinal <= resultadoSoma          when "000",
-    			       resultadoSubtracao     when "001",
-    			       resultadoIncremento    when "010",
-    			       resultadoCA2           when "011",
-    			       resultadoAnd           when "100",
-    			       resultadoComparador    when "101",
-    			       resultadoMultiplicacao when "110",
-    			       resultadoQuadrado      when "111",
-    			       "0000"                 when others;
+    -- Subtração
+    subtracao: FA4bits port map(entrada1, notEntrada2, '1', resultadoSubtracao, carrySubtracao, overflowSubtracao);
 
-	result <= resultFinal;
+    -- Incremento
+    incremento1: incremento port map(entrada1, resultadoIncremento, carryIncremento, overflowIncremento);
 
-	with operador select
-		flagCarry <= carrySoma               when "000",
-					 carrySubtracao          when "001",
-					 carryIncremento         when "010",
-					 '0'                     when others;
+    -- Complemento de 2
+    ComplementoA2: CA2 port map(entrada1, resultadoCA2);
 
-	flagNegativo <= resultFinal(3);
-	flagZero <= not(resultFinal(3) or resultFinal(2) or resultFinal(1) or resultFinal(0));
+    -- AND
+    resultadoAnd <= entrada1 and entrada2;
 
-	with operador select
-		flagOverflow <= overflowSoma          when "000",
-		                overflowSubtracao     when "001",
-						overflowIncremento    when "010",
-					    overflowMultiplicacao when "110",
-						overflowQuadrado      when "111",
-					    '0'                   when others;
+    -- Comparador
+    compara: comparador port map(entrada1, entrada2, resultadoComparador);
+
+    -- Multiplicação
+    multiplica: multiplier port map(entrada1, entrada2, overflowMultiplicacao, resultadoMultiplicacao);
+
+    -- Quadrado
+    quadrado: multiplier port map(entrada1, entrada1, overflowQuadrado, resultadoQuadrado);
+
+    -- Seleção do resultado com base no operador
+    with operador select
+        resultFinal <= resultadoSoma          when "000",
+                       resultadoSubtracao     when "001",
+                       resultadoIncremento    when "010",
+                       resultadoCA2           when "011",
+                       resultadoAnd           when "100",
+                       resultadoComparador    when "101",
+                       resultadoMultiplicacao when "110",
+                       resultadoQuadrado      when "111",
+                       "0000"                 when others;
+
+    -- Saída final
+    result <= resultFinal;
+
+    -- Definição da Flag
+    with operador select
+        flagCarry <= carrySoma               when "000",
+                     carrySubtracao          when "001",
+                     carryIncremento         when "010",
+                     '0'                     when others;
+
+    flagNegativo <= resultFinal(3);
+    flagZero <= not(resultFinal(3) or resultFinal(2) or resultFinal(1) or resultFinal(0));
+
+    with operador select
+        flagOverflow <= overflowSoma          when "000",
+                        overflowSubtracao     when "001",
+                        overflowIncremento    when "010",
+                        overflowMultiplicacao when "110",
+                        overflowQuadrado      when "111",
+                        '0'                   when others;
 end behavioral;
